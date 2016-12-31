914c5e4425f2f9029f958bc479378d027eb461e8
Carlos Mendible <Carlos Mendible>
2016-12-31 18:32:50 +0100

    retry pattern

---
+++ cloud.design.patterns/retry/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Retry.Sample
+{
+    using System;
+    using Polly;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var service = new UnreliableService();
+
+            var breaker = Policy
+                .Handle<TimeoutException>()
+                .Retry(2);
+
+            while (true)
+            {
+                var result = breaker.ExecuteAndCapture(
+                    () => service.GetTime()
+                );
+
+                if (result.Outcome == OutcomeType.Failure)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("Operation failed!!!"); 
+                    Console.ForegroundColor = ConsoleColor.White; 
+                }
+            }
+        }
+    }
+}
---
+++ cloud.design.patterns/retry/UnreliableService.cs
@@ -0,0 +1,22 @@
+﻿namespace Retry.Sample
+{
+    using System;
+    using System.Threading;
+
+    class UnreliableService
+    {
+        static readonly Random Random = new Random();
+        public DateTime GetTime()
+        {
+            if (Random.Next(10) == 1 || Random.Next(10) == 5)
+            {
+                Console.WriteLine("UnreliableService is unreliable");
+                Thread.Sleep(500);
+                throw new TimeoutException();
+            }
+
+            Console.WriteLine("UnreliableService is working");
+            return DateTime.Now;
+        }
+    }
+}
