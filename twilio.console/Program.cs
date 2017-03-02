5e4f16c1e9ea3570d40fcceaa1983100eb03b992
Carlos Mendible <carlos.mendible@sogeti.com>
2017-03-02 09:52:55 +0100

    added asyn method

--- twilio.console/Program.cs
+++ twilio.console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 using Twilio.Types;
@@ -9,23 +10,38 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
+            // Send the SMS
+            var messageSid = Task.Run(() => 
+            { 
+                return SendSms(); 
+            })
+            .GetAwaiter()
+            .GetResult();
+
+            // Write the message Id to the console.
+            Console.WriteLine(messageSid);
+            Console.Read();
+        }
+
+        // Send SMS with async feature!
+        private static async Task<string> SendSms()
+        {
             // Your Account SID from twilio.com/console
             var accountSid = "[Account SID]";
-            
+
             // Your Auth Token from twilio.com/console
-            var authToken = "[Auth Token]";   
+            var authToken = "[Auth Token]";
 
             // Initialize Twilio Client
             TwilioClient.Init(accountSid, authToken);
 
-            // Create & Send Message
-            var message = MessageResource.Create(
+            // Create & Send Message (New lib supports async await)
+            var message = await MessageResource.CreateAsync(
                 to: new PhoneNumber("[Target Phone Number]"),
                 from: new PhoneNumber("[Your Twilio Phone Number]"),
                 body: "Hello from dotnetcore");
 
-            Console.WriteLine(message.Sid);
-            Console.Read();
+            return message.Sid;
         }
     }
 }
