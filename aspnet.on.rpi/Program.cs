5939a7dae2a825fbd5f114790efb981a5a527299
Carlos Mendible <Carlos Mendible>
2017-03-21 12:23:27 +0100

    aspnetcore on rpi sample

---
+++ aspnet.on.rpi/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace aspnet.on.rpi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseUrls("http://*:5000") // Using * to bind to all network interfaces
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
