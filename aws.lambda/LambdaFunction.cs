e4a860f7dc5bd7eba2d5bb679b991d97639b1eeb
Carlos Mendible <cmendible@gmail.com>
2017-07-03 22:03:45 +0200

    aws lambda

---
+++ aws.lambda/LambdaFunction.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using System.IO;
+
+namespace aws.lambda
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .UseApiGateway();
+        }
+    }
+}
