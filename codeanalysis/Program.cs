8f619c34b5a4b53074218c78de44bfcbb51c410e
cmendible <cmendible@gmail.com>
2017-08-24 18:32:57 +0200

    CodeAnalysis and StyleCop sample

---
+++ codeanalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace codeanalysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
