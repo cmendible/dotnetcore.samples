47239cffc49739fb6f1244bd85aaa016ec749d57
cmendible <cmendible@gmail.com>
2017-09-01 11:22:56 +0200

    aspnet web api raspberry gpio sample

---
+++ aspnet.webapi.rpi.gpio/Controllers/BlinkyController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Unosquare.RaspberryIO;
+using Unosquare.RaspberryIO.Gpio;
+
+namespace aspnet.webapi.rpi.gpio.Controllers
+{
+    [Route("api/[controller]")]
+    public class BlinkyController : Controller
+    {
+        [HttpPost]
+        public void Post([FromBody]bool isOn)
+        {
+            var pin = Pi.Gpio.Pin05;
+            pin.PinMode = GpioPinDriveMode.Output;
+            pin.Write(!isOn);
+        }
+    }
+}
