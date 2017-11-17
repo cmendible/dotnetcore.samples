0a3d623d820b10470137c23ecfe8a18a80e136b3
cmendible <cmendible@gmail.com>
2017-11-17 12:48:26 +0100

    aspnet.https.on.docker sample

---
+++ aspnet.https.on.docker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_core.https.docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ aspnet.https.on.docker/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace aspnet_core.https.docker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            // Calling UseKestrel to set https configuration
+            WebHost.CreateDefaultBuilder(args)
+            .UseKestrel(options => 
+            {
+                options.Listen(IPAddress.Any, 443, listenOptions =>
+                {
+                    var configuration = (IConfiguration)options.ApplicationServices.GetService(typeof(IConfiguration));
+
+                    listenOptions.UseHttps("cert.pfx", configuration["certPassword"]);
+                });
+            })
+            .UseStartup<Startup>()
+            .Build();
+    }
+}
