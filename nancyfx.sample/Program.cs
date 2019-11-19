91412fb2a636f120a57400d4c82e18b1d8a0ec3a
Carlos Mendible <carlos.mendible.porras@repsol.com>
2019-11-19 17:21:17 +0100

    Upgraded to net core 3.0

--- nancyfx.sample/Program.cs
+++ nancyfx.sample/Program.cs
@@ -1,5 +1,5 @@
-using System.IO;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace WebApplication
 {
@@ -7,14 +7,14 @@ namespace WebApplication
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
     }
 }
--- nancyfx.sample/Startup.cs
+++ nancyfx.sample/Startup.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.Extensions.DependencyInjection;
 using Nancy.Owin;
 
 namespace WebApplication
 {
     public class Startup
     {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // If using Kestrel:
+            services.Configure<KestrelServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+
+            // If using IIS:
+            services.Configure<IISServerOptions>(options =>
+            {
+                options.AllowSynchronousIO = true;
+            });
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
